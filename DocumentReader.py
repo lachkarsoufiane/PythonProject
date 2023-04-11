56f1d000f4a9102a847cad25574510858dee4428
IVAN SOBRINO PALMA <beca_is2@altertechnology.com>
2023-04-11 10:03:34 +0200

    Progresion DocumentReader

--- DocumentReader.py
+++ DocumentReader.py
@@ -1,8 +1,9 @@
 import pdfplumber
 from collections import namedtuple
 import re
+import pandas as pd
 
-doc1_path = "./Files/esccrpqpl005iss224_jan22.pdf"
+doc1_path = "./Files/esccrpqpl005iss234_jan_23.pdf"
 change_descriptions = ["Extension", "Extension with new Remark",
                        "Extension with re-scope", "Editorial", "Removal", "Revision"]
 change_notice_pages = 3
@@ -10,8 +11,8 @@ change_notice_pages = 3
 data_re = re.compile(
     r'(\d{3}[a-zA-Z])([rev]{3}\d)?,\s(\d{3}[a-zA-Z])?([rev]{3}\d)?([a-zA-Z!@#$&()\\-`+,\’ ]+)\(([a-zA-Z]+)\)')
 
-data_re2 = re.compile(
-    r'((\d{3}[a-zA-Z])([rev]{3}\d)?,\s)*([a-zA-Z!@#$&()\\-`+\’ ]+)\(([a-zA-Z]+)\)')
+# data_re2 = re.compile(
+#     r'((\d{3}[a-zA-Z])([rev]{3}\d)?,\s)*([a-zA-Z!@#$&()\\-`+\’ ]+)\(([a-zA-Z]+)\)')
 
 
 def get_content(doc_1, doc_page):
@@ -37,13 +38,34 @@ def filter_content(content):
                 if not current_desc in change_descriptions_dictionary:
                     change_descriptions_dictionary[current_desc] = ""
         if (current_desc):
-            change_descriptions_dictionary[current_desc] +=  "_" + line
+            # change_descriptions_dictionary[current_desc] +=  "_" + line
+            
+            if x:
+             change_descriptions_dictionary[current_desc] +=  "_" + line
     
     return change_descriptions_dictionary
 
 
+def create_table(filter_table):
+    table = namedtuple('table', 'Certificate Revisio Munifacture Action Extra')
+    content = []
+
+    for key in filter_table:
+        for line in filter_table[key].split("_"):
+                line = data_re.search(line)
+
+                if line:
+                    cert = line.group(1)
+                    rev = line.group(2)
+                    mun = line.group(5) + " " + line.group(6)
+                
+                    content.append(table(cert, rev, mun, key, "-"))
+
+    return content
+
 table = get_content(doc1_path, change_notice_pages)
 filter_table = filter_content(table)
+content_table = create_table(filter_table)
 
-for key in filter_table:
-    print(data_re.findall(filter_table[key]))
+df = pd.DataFrame(content_table)
+print(df)
