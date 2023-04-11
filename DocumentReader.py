fa7e7971e67da1b3932ee7706a1f5f3c813ca9f9
IVAN SOBRINO PALMA <beca_is2@altertechnology.com>
2023-04-11 08:07:20 +0200

    DocumentReader actualizado

--- DocumentReader.py
+++ DocumentReader.py
@@ -2,10 +2,9 @@ import pdfplumber
 from collections import namedtuple
 import re
 
-doc1_path = "./Files/esccrpqpl005iss235.pdf"
+doc1_path = "./Files/esccrpqpl005iss224_jan22.pdf"
 change_descriptions = ["Extension", "Extension with new Remark",
                        "Extension with re-scope", "Editorial", "Removal", "Revision"]
-change_descriptions_dictionary = {}
 change_notice_pages = 3
 
 data_re = re.compile(
@@ -17,9 +16,19 @@ data_re2 = re.compile(
 
 def get_content(doc_1, doc_page):
     doc1_reader = pdfplumber.open(doc_1)
-    change_notice_page_content = doc1_reader.pages[doc_page -
-                                                   1].extract_table()
+    change_notice_page_content = doc1_reader.pages[doc_page -1].extract_table()
     content = change_notice_page_content[1][1]
+    return content
+
+
+    # print(data_re.findall(
+    #     change_descriptions_dictionary["Extension"]))
+
+    # print(data_re.findall(
+    #     "301Frev2, 300Drev3, Souriau (France)_365Arev2, Axon’ Cable (France)"))
+
+def filter_content(content):
+    change_descriptions_dictionary = {}
 
     for line in content.split('\n'):
         for description in change_descriptions:
@@ -28,13 +37,13 @@ def get_content(doc_1, doc_page):
                 if not current_desc in change_descriptions_dictionary:
                     change_descriptions_dictionary[current_desc] = ""
         if (current_desc):
-            change_descriptions_dictionary[current_desc] += "_" + line
-
-    # print(data_re.findall(
-    #     change_descriptions_dictionary["Extension"]))
-
-    print(data_re.findall(
-        "301Frev2, 300Drev3, Souriau (France)_365Arev2, Axon’ Cable (France)"))
+            change_descriptions_dictionary[current_desc] +=  "_" + line
+    
+    return change_descriptions_dictionary
 
 
-get_content(doc1_path, change_notice_pages)
+table = get_content(doc1_path, change_notice_pages)
+filter_table = filter_content(table)
+
+for key in filter_table:
+    print(data_re.findall(filter_table[key]))
